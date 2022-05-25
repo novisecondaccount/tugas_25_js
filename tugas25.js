61fe156e2f97cef469cdbb53c5d0448bb047997a
novifitri <animangap@gmail.com>
2022-05-25 19:05:33 +0700

    init

---
+++ tugas25.js
@@ -0,0 +1,6 @@
+let array = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16 ]
+console.log("Sebelumnya : "+array)
+console.log("Ascending : "+array.sort())
+console.log("Descending : "+array.reverse())
+let output = array.filter((item)=> item > 10)
+console.log("Filter > 10 : "+output)
